d49100fb44f67731c7ba370653aae28b5b24a080
Nayrobi Valencia Pino <nakavapi.30@gmail.com>
2023-09-13 08:35:47 -0500

    Se subr eproyecto de react sobre componentes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+const App = () => {
+  return (
+    <main className="main">
+        <Header />
+        <h1>Holi again</h1>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => { 
+    return(
+       <header>
+            <h1>Logo</h1>
+            <nav className="menu">
+                <a href="">Enlace</a>
+                <a href="">Enlace</a>
+                <a href="">Enlace</a>
+                <a href="">Enlace</a>
+            </nav>
+            <button>Boton</button>
+       </header> 
+    )
+}
+export default Header
